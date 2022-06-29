b2d9a7688781326a2eb0819034df5cae1bdad131
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-06-28 20:49:00 -0500

    Corrected error in python-app.yml file.

--- crontab-automation/setup_cronjob.py
+++ crontab-automation/setup_cronjob.py
@@ -1,4 +1,4 @@
-'''
+"""
     setup_cronjob.py
 
 
@@ -10,7 +10,7 @@
         cronjob and write it. The cronjob created is named so that when 
         re-running this script any existing cronjob with the same name can be 
         removed before created the new cronjob.
-'''
+"""
 
 ################################################################################
 
@@ -22,8 +22,8 @@ from crontab import CronTab
 
 # The cronjob will be setup to run the command below and it will be given the name
 # assigned to job_name.
-command = 'echo hello'
-job_name = 'Article Recommender'
+command = "echo hello"
+job_name = "Article Recommender"
 
 # Run the job during the hours defined below.
 hour_range = [21, 22]
@@ -34,13 +34,13 @@ minute_on = [0, 30]
 ################################################################################
 
 # Modify the crontab file of the current user.
-with CronTab(user = True) as cron:
+with CronTab(user=True) as cron:
 
     # Remove any existing cronjob with the same name as job_name.
     job = cron.find_comment(job_name)
     cron.remove(job)
 
     # Write the new cronjob using the parameters defined above.
-    job = cron.new(command = command, comment = job_name)
+    job = cron.new(command=command, comment=job_name)
     job.hour.during(*hour_range)
     job.minute.on(*minute_on)
--- original_codebase/Email_Interface/Configure_Email.py
+++ original_codebase/Email_Interface/Configure_Email.py
@@ -3,19 +3,26 @@ import os
 import logging
 import traceback
 
+
 def get_email_credentials():
-    login_credentials_file_path = os.path.join(os.path.dirname(os.getcwd()), 'ExternalResources/AS_Login_Credentials.yaml')
+    login_credentials_file_path = os.path.join(
+        os.path.dirname(os.getcwd()), "ExternalResources/AS_Login_Credentials.yaml"
+    )
     login_credentials = dict()
-    with open(login_credentials_file_path, 'rt') as f:
+    with open(login_credentials_file_path, "rt") as f:
         login_credentials = yaml.safe_load(f.read())
-    return login_credentials['username'], login_credentials['password']
+    return login_credentials["username"], login_credentials["password"]
+
 
 def get_mailing_list() -> dict:
-    mailing_list_file_path = os.path.join(os.path.dirname(__file__), 'Mailing_List.yaml')
+    mailing_list_file_path = os.path.join(
+        os.path.dirname(__file__), "Mailing_List.yaml"
+    )
     mailing_list = dict()
-    with open(mailing_list_file_path, 'rt') as f:
+    with open(mailing_list_file_path, "rt") as f:
         mailing_list = yaml.safe_load(f.read())
     return list(mailing_list.keys())
+
 
 def format_html_element(article: dict) -> str:
     return f'<a href="{article["link"]}">{article["title"]}</a>'
