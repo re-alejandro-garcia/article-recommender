8aa030fef0fc8eaf6b25e3687a847d258d3b2c65
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-07-05 17:21:11 -0500

    Added setup script for web scraper cronjob. A script setting up a cronjob for running the web scraper was created because the web scraper will need to execute multiple times throughout the day. It will be easier to have a separate cronjob for the web scraper.

--- crontab-automation/setup_cronjob.py
+++ crontab-automation/setup_cronjob.py
@@ -20,16 +20,22 @@ from crontab import CronTab
 
 ###############################################################################
 
-# The cronjob will be setup to run the command below and it will be given the 
-# name assigned to job_name.
-command = "echo hello"
-job_name = "Article Recommender"
+# These are the commands that will be executed by crontab for running the
+# article recommender app and the web scraper.
+article_recommender_command = "execute article recommender (actual command pending)"
+web_scraper_command = "execute web scraper (actual command pending)"
 
-# Run the job during the hours defined below.
-hour_range = [21, 22]
+# These are the job names for each cronjob. These are used for finding and
+# removing the command when being replaced.
+article_recommender_job_name = "Article Recommender"
+web_scraper_job_name = "Web Scraper"
+
+# Run the jobs during the hours defined below.
+article_recommender_hour_range = [21, 22]
+web_scraper_hour_on = [0, 12, 18]
 
 # Run the job at the minutes defined below.
-minute_on = [0, 30]
+article_recommender_minute_on = [0, 30]
 
 ###############################################################################
 
@@ -37,10 +43,28 @@ minute_on = [0, 30]
 with CronTab(user=True) as cron:
 
     # Remove any existing cronjob with the same name as job_name.
-    job = cron.find_comment(job_name)
+    job = cron.find_comment(article_recommender_job_name)
     cron.remove(job)
 
     # Write the new cronjob using the parameters defined above.
-    job = cron.new(command=command, comment=job_name)
-    job.hour.during(*hour_range)
-    job.minute.on(*minute_on)
+    job = cron.new(
+        article_recommender_command=article_recommender_command,
+        comment=article_recommender_job_name,
+    )
+    job.hour.during(*article_recommender_hour_range)
+    job.minute.on(*article_recommender_minute_on)
+
+###############################################################################
+
+# Modify the crontab file of the current user.
+with CronTab(user=True) as cron:
+
+    # Remove any existing cronjob with the same name as job_name.
+    job = cron.find_comment(web_scraper_job_name)
+    cron.remove(job)
+
+    # Write the new cronjob using the parameters defined above.
+    job = cron.new(
+        article_recommender_command=web_scraper_command, comment=web_scraper_job_name
+    )
+    job.hour.on(*web_scraper_hour_on)
