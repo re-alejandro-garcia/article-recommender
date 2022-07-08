c9fac5212d00a07e5032c8c4962124eb39d4f79a
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-07-08 16:00:03 -0500

    Added getopt to setup_cronjob script. In the setup_cronjob script code was added to allow for command line options to be passed to the script. The options allow for specifying which cronjob to setup and whether to delete them.

--- crontab-automation/setup_cronjob.py
+++ crontab-automation/setup_cronjob.py
@@ -18,12 +18,34 @@
 # Documentation: https://pypi.org/project/python-crontab/
 from crontab import CronTab
 
+from getopt import getopt, GetoptError
+import sys
+
+###############################################################################
+
+# Get any command line arguments that may have been passed. The possible
+# command line options are as follows:
+# -a : setup the article recommender cronjob
+# -w : setup the web scraper recommender cronjob
+# -d : delete the selected cronjob
+try:
+    opts, args = getopt(sys.argv[1:], "awd", [])
+
+    # Turn the received options into a set of all received options instead of
+    # the default tuples returned by getopt.
+    options = {opt[0] for opt in opts}
+
+# If an invalid command line argument was received exit the script.
+except GetoptError:
+    print(f"Received invalid option/argument: {sys.argv[1 : ]}")
+    sys.exit()
+
 ###############################################################################
 
 # These are the commands that will be executed by crontab for running the
 # article recommender app and the web scraper.
 article_recommender_command = "execute article recommender (actual command pending)"
-web_scraper_command = "execute web scraper (actual command pending)"
+web_scraper_command = "~/Repositories/article-recommender/shell-scripts/run-web-scraper"
 
 # These are the job names for each cronjob. These are used for finding and
 # removing the command when being replaced.
@@ -39,32 +61,36 @@ article_recommender_minute_on = [0, 30]
 
 ###############################################################################
 
-# Modify the crontab file of the current user.
-with CronTab(user=True) as cron:
+# Setup the article recommender cronjob if the -a option was received.
+if "-a" in options:
+    with CronTab(user=True) as cron:
 
-    # Remove any existing cronjob with the same name as job_name.
-    job = cron.find_comment(article_recommender_job_name)
-    cron.remove(job)
+        # Remove any existing cronjob with the same name as job_name.
+        job = cron.find_comment(article_recommender_job_name)
+        cron.remove(job)
 
-    # Write the new cronjob using the parameters defined above.
-    job = cron.new(
-        article_recommender_command=article_recommender_command,
-        comment=article_recommender_job_name,
-    )
-    job.hour.during(*article_recommender_hour_range)
-    job.minute.on(*article_recommender_minute_on)
+        # Write the new cronjob using the parameters defined above if the -d
+        # option was not received.
+        if "-d" not in options:
+            job = cron.new(
+                command=article_recommender_command,
+                comment=article_recommender_job_name,
+            )
+            job.hour.during(*article_recommender_hour_range)
+            job.minute.on(*article_recommender_minute_on)
 
 ###############################################################################
 
-# Modify the crontab file of the current user.
-with CronTab(user=True) as cron:
+# Setup the web scraper cronjob if the -w option was received.
+if "-w" in options:
+    with CronTab(user=True) as cron:
 
-    # Remove any existing cronjob with the same name as job_name.
-    job = cron.find_comment(web_scraper_job_name)
-    cron.remove(job)
+        # Remove any existing cronjob with the same name as job_name.
+        job = cron.find_comment(web_scraper_job_name)
+        cron.remove(job)
 
-    # Write the new cronjob using the parameters defined above.
-    job = cron.new(
-        article_recommender_command=web_scraper_command, comment=web_scraper_job_name
-    )
-    job.hour.on(*web_scraper_hour_on)
+        # Write the new cronjob using the parameters defined above if the -d
+        # option was not received.
+        if "-d" not in options:
+            job = cron.new(command=web_scraper_command, comment=web_scraper_job_name)
+            job.hour.on(*web_scraper_hour_on)
