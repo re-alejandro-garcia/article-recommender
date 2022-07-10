4d1382e5135b908a15e07b2548bfddcfc56f673c
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-07-10 13:26:50 -0500

    Added error handling code to the web scraper main. In the main script of the web scraper added error handling code in case an unexpected error occurs. Additionally, code was added to utilize the newly added return value of the configure_logger function so that the logger is used only if configuration was successful.

--- web_scraper/__main__.py
+++ web_scraper/__main__.py
@@ -27,25 +27,62 @@ sys.path.append("logging")
 
 import logging
 import pandas as pd
+import traceback
 
 from configure_logger import configure_logger
+from env import email_address
 
 ###############################################################################
 
 if __name__ == "__main__":
+    try:
 
-    configure_logger()
-    logger = logging.getLogger("webScraper")
+        # Configure the logger using the configure_logger module. If the
+        # configuration was successful use the webScraper logger for this script.
+        logger_configured_successfully = configure_logger()
+        logger = None
+        if logger_configured_successfully:
+            logger = logging.getLogger("webScraper")
 
-    # read new emails for updates
+        # read new emails for updates
 
-    # Pull all the latest articles from each website and API in the database.
-    sites_and_apis = pd.read_csv("data/site-and-api-data.csv")
-    for source in sites_and_apis:
-        pass
+        # Pull all the latest articles from each website and API in the database.
+        sites_and_apis = pd.read_csv("data/site-and-api-data.csv")
+        for source in sites_and_apis:
+            pass
 
-    # prepare the article data
+        # prepare the article data
 
-    # write the data to the database
+        # write the data to the database
 
-    logger.info("Web scraper completed execution.")
+        # If the logger  was configured successfully log that execution of the
+        # web scraper script completed.
+        if logger_configured_successfully:
+            logger.info(
+                """Web scraper completed execution.
+
+            -------------------------------------------
+
+            """
+            )
+
+    # If an unhandled and unexpected error occurred.
+    except Exception as error:
+        message = """
+            An unexpected and unhandled error occurred in the Article 
+            Recommender Web Scraper. This error caused execution of the script 
+            to hault. The error has been logged and is also shown here.
+
+        """
+        error_message = "".join(
+            traceback.format_exception(None, error, error.__traceback__)
+        )
+
+        # Log the traceback of the error message.
+        if logger_configured_successfully:
+            logger.error(error_message)
+
+        # Send an email to the app owner containing the error message.
+        os.system(
+            f'echo {message + error_message} | mail -s "Article Recommender Web Scraper Error" {email_address}'
+        )
